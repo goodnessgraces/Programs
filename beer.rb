e276d35928007ff0f2210c6db4ce462abfe81ccb
Cornelia Wright <goodnessgraces@me.com>
2015-07-02 08:20:17 -0400

    End of Week Update

---
+++ beer.rb
@@ -0,0 +1,12 @@
+n = 99
+while n > 1
+puts "#{n} Bottles of beer on the wall."
+puts "#{n} Bottles of beer. Take one down, pass it around."
+n -= 1
+	puts "#{n} bottles of beer on the wall"
+	if n == 1
+		puts "#{n} Bottle of beer on the wall."
+		puts "#{n} Bottle of beer. Take one dowm pass it around."
+		puts "We're drunk because we drank all of the beers on the wall"
+end
+end
